571b096aa42bee4014bc89c45fc4d49beb27c6fc
vaishnavi birle <vaishnavibirle1133@gmail.com>
2021-05-20 16:38:49 +0530

    [Vaishnavi]ADDED:Input fields and Validated fields

---
+++ js/AddressBook.js
@@ -0,0 +1,46 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const fullname = document.querySelector('#fullName');
+    const textError = document.querySelector('.text-error');
+    fullname.addEventListener('input', function () {
+        if (fullname.value.length == 0) {
+            textError.textContent = "";
+            return
+        }
+        try {
+            (new Contact()).fullName = fullname.value;
+            textError.textContent = "";
+        } catch (e) {
+            textError.textContent = e;
+        }
+    });
+
+    const phoneNo = document.querySelector('#tel');
+    const phoneError = document.querySelector('.mobile-error');
+    phoneNo.addEventListener('input', function () {
+        if (phoneNo.value.length == 0) {
+            phoneError.textContent = "";
+            return
+        }
+        try {
+            (new Contact()).phoneNumber = phoneNo.value;
+            phoneError.textContent = "";
+        } catch (e) {
+            phoneError.textContent = e;
+        }
+    });
+
+    const address = document.querySelector('#address');
+    const addressError = document.querySelector('.address-error');
+    address.addEventListener('input', function () {
+        if (address.value.length == 0) {
+            addressError.textContent = "";
+            return
+        }
+        try {
+            (new Contact()).address = address.value;
+            addressError.textContent = "";
+        } catch (e) {
+            addressError.textContent = e;
+        }
+    });
+});
---
+++ js/Contact.js
@@ -0,0 +1,65 @@
+/**UC4 Validating all fields by creating contact class */
+
+class Contact {
+
+    /**validating first name 
+     * must start with capital letter and must have minimum three characters
+     */
+    get fullName() {return this._fullName;}
+    set fullName(fullName) {
+        let fullNamePattern = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+        if (fullNamePattern.test(fullName)) {
+            this._fullName = fullName;
+        }
+        else
+            throw 'Invalid Name';
+    }
+
+
+    /**Validating phone number
+     * consists of either 10 or 12 or 13 numbers
+     */
+
+    get tel() {return this._tel;}
+    set tel(tel) {
+        let telPattern = RegExp('((^\\+?)(([0-9]{2,3})(\\s))?)' + '[0-9]{10}$');
+        if (telPattern.test(tel)) {
+            this._tel = tel;
+        }
+        else
+            throw 'Invalid Mobile Number';
+    }
+
+    /**Validating address 
+     * must have multiple words and each word with min 3 characters
+     */
+
+    get address() { return this._address; }
+    set address(address) {
+        let words = address.split(" ");
+        let addressPattern = RegExp('([A-Z a-z 0-9]{3,})+');
+        for (const word of words) {
+            if (!addressPattern.test(word))
+                throw 'Invalid Address';
+        }
+        this._address = address;
+    }
+
+    get city() {return this._city;}
+    set city(city) {this._city = city;}
+
+    get state() {return this._state;}
+    set state(state) {this._state = state;}
+
+    get zip() {return this._zip;}
+    set zip(zip) {this._zip = zip;}
+
+    
+
+    //Methods
+    toString() {
+        return '[ FullName : ' + this.fullName + ' Address : '
+            + this.address + ' City : ' + this.city + ' State : ' + this.state + ' Zip : ' + this.zip +
+            ' Phone Number : ' + this.phoneNumber + ' ]';
+    }
+}
